7c1ea93d2d908ce6a8bf4e99114689f1608c9579
Johnny Tsai <zxp86021@gmail.com>
2019-08-09 20:56:54 +0800

    add config

---
+++ config/dog-cat.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'the_dog' => [
+
+        /**
+         * API Key For TheDogApi
+         * More Detail: https://docs.thedogapi.com/authentication
+         */
+        'api_key' => ''
+    ],
+
+    'the_cat' => [
+
+        /**
+         * API Key For TheCatApi
+         * More Detail: https://docs.thecatapi.com/authentication
+         */
+        'api_key' => ''
+    ],
+
+];
